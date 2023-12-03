c9554267c41b11200579aca54d36a6c1baa3d588
Maximus Coleman <max@maximuscoleman.com>
2023-12-03 19:13:46 +0000

    The JS will now generate the tiles, allows for expandability.

--- app.js
+++ app.js
@@ -6,19 +6,33 @@ const roundContainer = document.querySelector(".round");
 const tileContainer = document.querySelector(".tiles");
 const screens = document.querySelectorAll(".screen");
 
+// Word for the round gets saved here.
 let word;
 let round = 1;
-let row = 0;
-let rows = 5;
-let rowPosition = 0;
-let lettersPerRow = 5;
 let highscore = 0;
 
+// How many rows there are on the screen.
+let rows = 5;
+
+// How many tiles there are per row.
+let tilesPerRow = 5;
+
+// The current row the player is on.
+let activeRow = 0;
+
+// The position in the row the player is on.
+let rowPos = 0;
+
+// Each row letter inputted gets saved in the buffer.
 let rowBuffer;
 
+// Supports random or word of the day.
 async function fetchWord(random) {
   showLoading();
-  const query = `${WORD_URL}${random ? "?random=1" : ""}`;
+  let query = `${WORD_URL}`;
+
+  if (random) query += `${random ? "?random=1" : ""}`
+
   const promise = await fetch(query);
   const response = await promise.json();
   hideLoading();
@@ -37,14 +51,17 @@ async function isWord(word) {
   return response.validWord;
 }
 
+// Each key pressed gets pushed to the buffer.
 function processKeyUp(key) {
   if (!rowBuffer) return;
-  if (rowBuffer.length >= lettersPerRow) return;
+  if (rowBuffer.length >= tilesPerRow) return;
 
   rowBuffer.push(key.toUpperCase());
   forward();
 }
 
+// The main input listener, filters non letters 
+// and checks for enter and backspace first.
 function setupListener() {
   window.addEventListener("keyup", (e) => {
     const key = e.key.toLowerCase();
@@ -61,6 +78,8 @@ function setupListener() {
     processKeyUp(key);
   });
 
+  // Register listeners for each button.
+  // The addListener function handles the repetitive stuff.
   addListener(".btn-play", "click", () => {
     hideMenu();
     showTiles();
@@ -85,20 +104,46 @@ function setupListener() {
   });
 }
 
+// We repeatedly create listeners for the buttons, so this handles that.
 function addListener(selector, event, func) {
   document.querySelector(selector).addEventListener(event, func)
 }
 
+// I'm generating the tiles here instead of manually creating them in the HTML document.
+// Allows to expand the tiles just by changing the top variables.
+function setupTiles() {
+  let newRows = [];
+  
+  // We generate the row containers for the tiles.
+  for (let i = 0; i < rows + 1; i++) {
+    let row = document.createElement("div");
+    
+    row.classList.add("row");
+    tileContainer.prepend(row);
+    newRows.push(row);
+  }
+
+  // Now generate the tiles for each row.
+  newRows.forEach((r) => {
+    for (let i = 0; i < tilesPerRow; i++) {
+      let tile = document.createElement("div");
+    
+      tile.classList.add("tile");
+      r.append(tile);
+    }
+  })
+}
+
 async function setupRound() {
+  // Refresh and clear the buffer and tiles for each new round.
   clearBuffer();
   clearTiles();
 
-  row = 0;
-  rowPosition = 0;
+  activeRow = 0;
+  rowPos = 0;
 
   word = await fetchWord(true);
   roundContainer.innerText = "Round: " + round;
-  console.log("Word is " + word);
 }
 
 function winRound() {
@@ -111,9 +156,12 @@ function winRound() {
 
 function loseRound() {
   showLoseScreen();
+
+  // Rounds get reset back to 1 when you lose.
   round = 1;
 }
 
+// These handle visibility of the elements, by adding the .hide class.
 function hide(element) {
   element.classList.add("hide");
 }
@@ -122,30 +170,34 @@ function show(element) {
   element.classList.remove("hide");
 }
 
+// This checks the rows letters to see if any match the criteria.
 async function checkRow() {
-  if (rowPosition < lettersPerRow) return;
+  if (rowPos < tilesPerRow) return;
   let combined = rowBuffer.join("");
 
   let valid = await isWord(combined);
 
+
+  // Looping through each letter, checking if any match, then apply the styles
+  // and or the win and lose states.
   for (let i = 0; i < rowBuffer.length; i++) {
     if (!valid) {
-      updateTile(row, i, "red");
+      updateTile(activeRow, i, "red");
       continue;
     }
 
-    updateTile(row, i, "used");
+    updateTile(activeRow, i, "used");
     
     let letter = rowBuffer[i];
     let match = isMatch(letter, i);
 
     if (!match) continue;
     if (match == "green") {
-      updateTile(row, i, "green");
+      updateTile(activeRow, i, "green");
       continue;
     }
 
-    updateTile(row, i, "yellow");
+    updateTile(activeRow, i, "yellow");
   }
 
   if (!valid) return;
@@ -155,15 +207,16 @@ async function checkRow() {
     return;
   }
 
-  if (row == rows) {
+  if (activeRow == rows) {
     loseRound();
     return;
   }
 
-  row++;
+  activeRow++;
   clearBuffer();
 }
 
+// Returns a list of all the tile rows.
 function getRows() {
   let rows = [[], [], [], [], [], []];
 
@@ -192,26 +245,32 @@ function isMatch(letter, position) {
   return "yellow";
 }
 
+// When pressing enter, this will add the letter to the tile
+// then move the position over one.
 function forward() {
-  if (rowPosition >= 5) return;
+  if (rowPos >= 5) return;
   
-  updateTile(row, rowPosition, "add");
-  rowPosition++;
-  console.log(row, rowPosition, rowBuffer)
+  updateTile(activeRow, rowPos, "add");
+  rowPos++;
+  console.log(activeRow, rowPos, rowBuffer)
 }
 
+// When pressing backspace, this will remove the letter of the tile
+// then move the position back one.
 function backward() {
-  if (rowPosition == 0) return;
+  if (rowPos == 0) return;
   
   document.querySelectorAll(".red").forEach((e) => {
     e.classList.remove("red");
   });
 
-  updateTile(row, rowPosition - 1, "delete");
-  rowPosition--;
+  updateTile(activeRow, rowPos - 1, "delete");
+  rowPos--;
   rowBuffer.pop();
 }
 
+
+// Updates the tiles according to actions.
 function updateTile(row, position, action) {
   let rows = getRows();
   let tile = rows[row][position];
@@ -240,10 +299,13 @@ function updateTile(row, position, action) {
   }
 }
 
+// We need to be able to clear the left over tile classes between games
+// this function handles that for each tile passed.
 function clearTileClasses(tile) {
   tile.classList.remove("used-tile", "green", "yellow", "red");
 }
 
+// Remove all tile styles, used for new rounds.
 function clearTiles() {
   let rows = getRows();
   
@@ -256,8 +318,10 @@ function clearTiles() {
 
 function clearBuffer() {
   rowBuffer = [];
-  rowPosition = 0;
+  rowPos = 0;
 }
+
+// A bunch of functions to control visibility of different sections.
 
 function showMenu() {
   show(menu);
@@ -270,10 +334,12 @@ function hideMenu() {
 
 function showTiles() {
   show(tileContainer);
+  show(document.querySelector(".round"));
 }
 
 function hideTiles() {
   hide(tileContainer);
+  hide(document.querySelector(".round"));
 }
 
 function showWinScreen() {
@@ -306,4 +372,6 @@ function displayWord(element) {
   element.innerText = word;
 }
 
+// Sets up the event listeners and generates tiles using HTML divs.
 setupListener();
+setupTiles();
